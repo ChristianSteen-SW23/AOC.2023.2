e7e2796f71a9d0d4ee2369aa9bfd85d4824527f0
ChristianSteen-SW23 <cniel23@student.aau.dk>
2023-12-13 19:56:04 +0100

    Add project files.

---
+++ AOC.2023.2/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace AOC._2023._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        static string[] ReadFile(string filePath)
+        {
+            try
+            {
+                int lineCount = 0;
+                // Finds the amount of lines in a the data file
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    while (sr.ReadLine() != null)
+                    {
+                        lineCount++;
+                    }
+                }
+
+                Console.WriteLine("Line count is: " + lineCount);
+
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    // Sets up the array to storge the data
+                    string[] lines = new string[lineCount];
+
+                    // Makes a forloop to scan in all the data
+                    for (int i = 0; i < lineCount; i++)
+                    {
+                        lines[i] = sr.ReadLine();
+                    }
+                    /*foreach (string line in lines)
+                    {
+                        Console.WriteLine(line);
+                    }*/
+                    return lines;
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Did not find the file");
+                Environment.Exit(-1);
+                return null;
+            }
+        }
+    }
+}
